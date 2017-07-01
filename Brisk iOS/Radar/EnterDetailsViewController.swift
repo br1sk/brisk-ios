997d14bf56b6543a3e680a9f41f8396d2fabd24d
Florian Buerger <florian@florianbuerger.com>
2017-07-01 16:20:04 -0600

    Implement placeholder for UITextView

--- Brisk iOS/Radar/EnterDetailsViewController.swift
+++ Brisk iOS/Radar/EnterDetailsViewController.swift
@@ -4,6 +4,13 @@ import InterfaceBacked
 final class EnterDetailsViewController: UIViewController, StoryboardBacked {
 
 
+	// MARK: - Types
+
+	enum Styling {
+		case normal, placeholder
+	}
+
+
 	// MARK: - Properties
 
 	@IBOutlet weak var textView: UITextView!
@@ -22,6 +29,49 @@ final class EnterDetailsViewController: UIViewController, StoryboardBacked {
 		} else {
 			textView.text = prefilledContent
 			textView.textColor = UIColor.darkText
+		}
+	}
+
+
+	// MARK: - Private Methods
+
+	fileprivate func moveCursorToStart() {
+		DispatchQueue.main.async {
+			self.textView.selectedRange = NSMakeRange(0, 0)
+		}
+	}
+
+	fileprivate func applyStyling(_ styling: Styling) {
+		switch styling {
+		case .normal:
+			textView.textColor = UIColor.darkText
+		case .placeholder:
+			textView.text = placeholder
+			textView.textColor = UIColor.lightGray
+		}
+	}
+}
+
+extension EnterDetailsViewController: UITextViewDelegate {
+
+	func textViewDidBeginEditing(_ textView: UITextView) {
+		if textView.text == placeholder {
+			moveCursorToStart()
+		}
+	}
+
+	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+		let length = textView.text.characters.count + text.characters.count - range.length
+		if length > 0 {
+			if textView.text == placeholder {
+				applyStyling(.normal)
+				textView.text = ""
+			}
+			return true
+		} else {
+			applyStyling(.placeholder)
+			moveCursorToStart()
+			return false
 		}
 	}
 }

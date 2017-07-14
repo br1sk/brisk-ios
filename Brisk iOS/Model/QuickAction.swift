fc10b47837eee227aa2716507c68d588d20514f7
Florian Buerger <florian@florianbuerger.com>
2017-07-14 17:18:00 -0600

    Fix whitespace

--- Brisk iOS/Model/QuickAction.swift
+++ Brisk iOS/Model/QuickAction.swift
@@ -11,16 +11,16 @@ public extension QuickAction {
     struct Shortcut {
         static var new: UIApplicationShortcutItem {
             let icon = UIApplicationShortcutIcon(templateImageName: "Compose")
-            
+
             return UIApplicationShortcutItem(type: "new-radar",
                                              localizedTitle: NSLocalizedString("QuickAction.NewRadar", comment: ""),
                                              localizedSubtitle: nil,
                                              icon: icon)
         }
-        
+
         static var duplicate: UIApplicationShortcutItem {
             let icon = UIApplicationShortcutIcon(templateImageName: "Duplicate")
-            
+
             return UIApplicationShortcutItem(type: "duplicate-radar",
                                              localizedTitle: NSLocalizedString("QuickAction.DuplicateRadar", comment: ""),
                                              localizedSubtitle: nil,
